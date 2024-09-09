f6f6fb7334ffe6d95eefd2c47f8893b7e6b238bd
DaniloO.Pinheiro <daniloopro@gmail.com>
2024-09-09 15:05:22 -0300

    O que são condicionais e booleanos

--- portifolio/index.php
+++ portifolio/index.php
@@ -13,22 +13,63 @@
 
         $titulo = $saudacao . " Portfolio " . $nome;
 
-        $subtitulo = "Seja bem vindo ao seu portfolio!!";
+        $subtitulo = "Seja bem vindo ao seu portifolio!!";
 
-        $ano = 2024;
+        $ano = 2020;
+
+        $projeto="Meu Portifolio";
+
+        $finalizado=false;
+
+        $dataDoProjeto="2024-10-11";
+
+        $descricao="Meu primeiro portifolio. Escrito em PHP e HTML";
     ?>
 
-    <h1>
-        <?=$titulo?>
-    </h1>
+    <h1><?=$titulo?></h1>
 
-    <p>
-        <?=$subtitulo?>
-    </p>
+    <p><?=$subtitulo?></p>
 
-    <p>
-        <?=$ano?>
-    </p>
+    <p><?=$ano?></p>
+
+    <hr/>
+
+    <div 
+
+        <?php if( ! ((2024 - $ano) > 2) ): ?>
+            style="background-color: burlywood;"
+        <?php endif; ?>
+    >
+
+        <h2><?=$projeto?></h2>
+        <p><?=$descricao?></p>
+
+        <div>
+            <div><?=$dataDoProjeto?></div>
+
+            <div> Projeto:
+
+                <?php if( ! $finalizado): ?>
+                    
+                    <span style="color: red;">Não Finalizado</span>
+                    
+                    <?php else: ?>
+                        
+                        <span style="color: green;">Finalizado</span>
+                    
+                <?php endif;?>
+
+
+                <?php 
+                    if( $finalizado ){
+                        echo "Finalizado!";
+                    } else {
+                        echo "Não Finalizado!";
+                    }
+                ?>
+            </div>
+        </div>
+    </div>
 
 </body>
 </html>
