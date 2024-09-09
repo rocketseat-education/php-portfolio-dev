918b904658e757ab3701ac28339c919084bbd62f
DaniloO.Pinheiro <daniloopro@gmail.com>
2024-09-09 13:46:40 -0300

    Primeira tag PHP

---
+++ portifolio/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Protfolio</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Portfolio do Aluno";
+        ?>
+    </h1>
+
+    <p>
+        <?php 
+        echo "Nome do Aluno";
+        ?>
+    </p>
+
+
+</body>
+</html>
